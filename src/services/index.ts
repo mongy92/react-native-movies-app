5ace17d638b286cca22ac4519e58deb3d72193b8
Ahmed Mongy <a.elzantot@gmail.com>
2021-06-10 02:27:33 +0200

    write requests for fetching movies & genres

---
+++ src/services/index.ts
@@ -0,0 +1,32 @@
+import { MoviesFilter } from '../utils/types';
+import axios_client from './axios_client';
+
+export interface FetchMoviesParams {
+  filter: MoviesFilter;
+  page: number;
+}
+
+export const fetchMoviesList = async (params: FetchMoviesParams) => {
+  const { filter, page } = params;
+
+  try {
+    const response = await axios_client.get(`movie/${filter}`, {
+      params: {
+        page,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const fetchGenres = async () => {
+  try {
+    const response = await axios_client.get('genre/movie/list');
+
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
