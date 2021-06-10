5a4a14236f46f4169a245da402a655520d750313
Ahmed Mongy <a.elzantot@gmail.com>
2021-06-10 17:10:05 +0200

    add style prop for GenresList component

--- src/components/GenresList/index.tsx
+++ src/components/GenresList/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleProp, ViewStyle } from 'react-native';
 import { Genre } from '../../utils';
 import styles from './styles';
 
 interface Props {
   genres: Genre[];
+  style?: StyleProp<ViewStyle>;
 }
 
-const GenresList: React.FC<Props> = ({ genres }) => {
+const GenresList: React.FC<Props> = ({ genres, style }) => {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, style]}>
       {genres.map((g) => (
         <View style={styles.genre} key={g.id}>
           <Text style={styles.genreText}>{g.name}</Text>
