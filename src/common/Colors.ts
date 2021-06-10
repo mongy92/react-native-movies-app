b59bf58c43f97402916dd9b97ed278ce04361d2e
Ahmed Mongy <a.elzantot@gmail.com>
2021-06-10 15:44:18 +0200

    style for MovieDetails header

--- src/common/Colors.ts
+++ src/common/Colors.ts
@@ -1,7 +1,7 @@
 export const COLORS = {
   green: '#33c500',
   light_gray: '#d8d8d8',
-  dark: '#555',
+  dark: '#333',
   gray: '#999',
   white: '#FFFFFF',
   black: '#000000',
--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -5,6 +5,7 @@ import MoviesList from '../screens/MoviesList';
 import MovieDetails from '../screens/MovieDetails';
 import styles from './styles';
 import { Genre, Movie } from '../utils';
+import { COLORS } from '../common';
 
 export type MainStackParams = {
   Splash: undefined;
@@ -19,9 +20,19 @@ const MainStack = createStackNavigator<MainStackParams>();
 
 export default () => (
   <MainStack.Navigator
-    screenOptions={{ headerTitleAlign: 'left', headerTitleStyle: styles.headerTitleStyle }}>
+    screenOptions={{
+      headerTitleAlign: 'left',
+      headerTitleStyle: styles.headerTitleStyle,
+      headerTintColor: COLORS.dark,
+      headerTruncatedBackTitle: '',
+      headerBackTitle: '',
+    }}>
     <MainStack.Screen component={Splash} name="Splash" options={{ headerShown: false }} />
     <MainStack.Screen component={MoviesList} name="MoviesList" options={{ title: 'Movies' }} />
-    <MainStack.Screen component={MovieDetails} name="MovieDetails" />
+    <MainStack.Screen
+      component={MovieDetails}
+      name="MovieDetails"
+      options={{ headerTransparent: true, title: '' }}
+    />
   </MainStack.Navigator>
 );
